1f514999736b0d9a544a11a4271bb5a5b585ebf1
itzzSVR-tech <marioplayz1975@gmail.com>
2025-12-06 12:42:31 +0530

    Day 4 recent commit

---
+++ Day4/recursiveSumDigits.java
@@ -0,0 +1,30 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class recursiveSumDigits {
+    static int sumDigits(int n) {
+        if (n < 0) {
+            n = -n;
+        }
+        if (n == 0) {
+            return 0;
+        }
+
+        return (n % 10) + sumDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int result = sumDigits(number);
+
+        System.out.println("Number: " + number);
+        System.out.println("Sum: " + result);
+
+        sc.close();
+    }
+}
