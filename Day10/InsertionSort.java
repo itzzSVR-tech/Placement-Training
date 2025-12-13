53716a2af7e3bf67ef65030e1d92c94632a966fc
itzzSVR-tech <marioplayz1975@gmail.com>
2025-12-13 14:26:10 +0530

    Day10 recent

---
+++ Day10/InsertionSort.java
@@ -0,0 +1,27 @@
+package Day10;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        System.out.println("Unsorted Array: " + Arrays.toString(arr));
+        insertionSort(arr);
+        System.out.println("Sorted Array:   " + Arrays.toString(arr));
+    }
+}
---
+++ Day10/SelectionSort.java
@@ -0,0 +1,28 @@
+package Day10;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Unsorted Array: " + Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println("Sorted Array:   " + Arrays.toString(arr));
+    }
+}
